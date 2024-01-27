0d7eb985b60696736472ca134005772c8304e8bd
Andrey Menshikov <andrey.menshikov@bostongene.com>
2024-01-27 23:25:11 +0400

    half app with video

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './App.css'
+import BoardComponent from './component/BoardComponent';
+import { Board } from './models/Board';
 
 const App = () => {
-  return (
-    <div>
+  const [board, setBoard] = useState(new Board())
+  useEffect(() => {
+    restart()
+  }, [])
 
+  function restart() {
+    const newBoard = new Board()
+    newBoard.initCell()
+    newBoard.addFigures()
+    setBoard(newBoard)
+  }
+  return (
+    <div className='App'>
+      <BoardComponent board={board} setBoard={setBoard} />
     </div>
   );
 };
